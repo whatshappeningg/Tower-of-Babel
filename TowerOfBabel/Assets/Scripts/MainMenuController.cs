daf6ea837c92650e8e2b4ee7155caad49b673c43
Althea <althea.larrauri@gmail.com>
2026-04-21 17:40:49 +0200

    feat: crear menú principal básico con script (funcional pero feo)

---
+++ TowerOfBabel/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    #region Properties
+    #endregion
+
+    #region Fields
+    [SerializeField] Button _startGameButton;
+    [SerializeField] Button _exitGameButton;
+    #endregion
+
+    #region Unity Callbacks
+    void Start()
+    {
+        _startGameButton.onClick.AddListener(StartGame);
+        _exitGameButton.onClick.AddListener(ExitGame);
+    }
+
+
+    #endregion
+
+    #region Private Methods
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+    #endregion
+}
